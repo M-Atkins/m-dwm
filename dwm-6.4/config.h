110dce469c92c6634cb28afbdbd88d43db3b3ad7
m-atkins <matthewatkins16@gmail.com>
2023-11-09 11:50:44 +0000

    update

--- dwm-6.4/config.h
+++ dwm-6.4/config.h
@@ -103,7 +103,8 @@ static const Layout layouts[] = {
 #define STATUSBAR "dwmblocks"
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+//static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "rofi", "-show", "drun", "-show-icons",NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
